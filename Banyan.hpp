d42cbbaec21d69b38235c50f7c761db5751e904d
bhallstein <a@ben.am>
2024-01-16 15:44:40 +0000

    move Map into Banyan.hpp

--- Banyan.hpp
+++ Banyan.hpp
@@ -5,9 +5,37 @@
 #include <string>
 #include <vector>
 
-#include "map.hpp"
-
 namespace Banyan {
+
+
+// Map
+// ------------------------------------
+// - compiles faster than std::map and in testing provides marginally
+//   better performance (!)
+
+template <class T>
+struct Map {
+  struct Entry {
+    std::string name;
+    T           item;
+  };
+
+  std::vector<Entry> entries;
+
+  Map() {}
+  Map(std::initializer_list<Entry> e) : entries(e) {}
+
+  T& operator[](std::string name) {
+    for (auto& entry : entries) {
+      if (entry.name == name) {
+        return entry.item;
+      }
+    }
+
+    entries.push_back({name});
+    return entries.back().item;
+  }
+};
 
 
 // Definitions
